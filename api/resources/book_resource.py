8ca17da94c324875534c73d19930ce8828ef9054
AleksMD <budjonniy291@gmail.com>
2019-12-08 20:38:56 +0200

    Create test for book entity. Create book entity that allows to add new book to DB

--- api/resources/book_resource.py
+++ api/resources/book_resource.py
@@ -1,21 +1,49 @@
+from flask import request
 from flask_restful import Resource, marshal_with
 from api.db_models.book_model import Books
 from api.structures.book_structure import book_structure
+from extensions import db
 
 
 class Book(Resource):
     @marshal_with(book_structure)
-    def get(self):
-        return Books.query.all()
+    def get(self, id_=None):
+        if id_:
+            return Books.query.filter_by(id=id_).first_or_404()
+        data = request.get_json()
+        return Books.query.filter_by(**data).all() if data and any(data.values()) else Books.query.all()
 
     def post(self):
-        return 'ok/Post'
+        data = request.get_json()
+        print(data)
+        if data:
+            db.session.add(Books(**data))
+            db.session.commit()
+            return 200
+        return 'All fields should be fulfilled'
 
-    def put(self):
-        return 'Inside Put. Ok'
+    def put(self, id_=None):
+        data = request.get_json()
+        if id_ and data:
+            book_to_update = Books.query.filter_by(id=id_).first_or_404()
+            for key, value in data.items():
+                setattr(book_to_update, key, value)
+            return 200
+        return 'Sorry something went wrong'
 
-    def pathc(self):
-        return 'Inside Patch. OK'
+    def patch(self, id_=None):
+        data = request.get_json()
+        if id_ and data:
+            book_to_update = Books.query.filter_by(id=id_).first_or_404()
+            for key, value in data.items():
+                setattr(book_to_update, key, value)
+            return 200
+        return 'Sorry something went wrong'
 
-    def delete(self):
-        return 'Inside Delete. Ok'
+    def delete(self, id_=None):
+        if id_:
+            book_to_delete = Books.query.filter_by(id=id_).first_or_404()
+            db.session.delete(book_to_delete)
+            db.session.commit()
+            return 200
+        return 'Sorry something went wrong'
--- tests/test_book.py
+++ tests/test_book.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
+import unittest
 from extensions import db
 from api.db_models.book_model import Books
-from main import create_app, get_config
+from main import create_app
+import json
 
 
 class TestBookEntity(TestCase):
@@ -21,6 +23,17 @@ class TestBookEntity(TestCase):
                       'year_of_publication': 2014,
                       'edition': '2nd',
                       'translator': None}
+        self.book3 = {'id': 3,
+                      'name': 'Data Structures and Algorithm Analysis in C++',
+                      'author': 'Mark Allen Weiss',
+                      'year_of_publication': 2005,
+                      'edition': '3rd',
+                      'translator': None}
+        self.book4 = {'name': 'Test',
+                      'author': 'Jane Doe',
+                      'year_of_publication': 1970,
+                      'edition': '1st',
+                      'translator': 'John Doe'}
         db.session.add(Books(**self.book1))
         db.session.add(Books(**self.book2))
         db.session.commit()
@@ -33,3 +46,50 @@ class TestBookEntity(TestCase):
         resp_get_book = self.app.test_client().get('/books')
         self.assertEqual(resp_get_book.json, [self.book1, self.book2])
         self.assertEqual(resp_get_book.status_code, 200)
+
+    def test_get_particular_book_by_id(self):
+        resp_get_book = self.app.test_client().get('/books/1')
+        self.assertEqual(resp_get_book.json, self.book1)
+        self.assertEqual(resp_get_book.status_code, 200)
+
+    def test_search_book_by_filters(self):
+        filters = {'name': 'The Overstory',
+                   'author': 'Richard Powers'}
+        resp_get_book = self.app.test_client().get('/books', data=json.dumps(filters),
+                                                   content_type='application/json')
+        self.assertEqual(resp_get_book.json, [self.book1])
+        self.assertEqual(resp_get_book.status_code, 200)
+
+    def test_post_new_book(self):
+        resp_post_book = self.app.test_client().post('/books', data=json.dumps(self.book3),
+                                                     content_type='application/json')
+        resp_get_book = self.app.test_client().get('/books')
+        self.assertEqual(resp_post_book.status_code, 200)
+        self.assertEqual(resp_get_book.json, [self.book1, self.book2, self.book3])
+
+    def test_partially_modifying_existing_book(self):
+        data_to_be_changed = {'translator': 'Cambridge linguistic center'}
+        resp_patch_book = self.app.test_client().patch('/books/1', data=json.dumps(data_to_be_changed),
+                                                       content_type='application/json')
+        resp_get_book = self.app.test_client().get('/books/1')
+        self.book1.update(data_to_be_changed)
+        self.assertEqual(resp_patch_book.status_code, 200)
+        self.assertEqual(resp_get_book.json, self.book1)
+
+    def test_full_replacement_of_the_book(self):
+        resp_put_book = self.app.test_client().put('/books/1', data=json.dumps(self.book4),
+                                                   content_type='application/json')
+        resp_get_book = self.app.test_client().get('/books/1')
+        self.book1.update(self.book4)
+        self.assertEqual(resp_put_book.status_code, 200)
+        self.assertEqual(resp_get_book.json, self.book1)
+
+    def test_delete_the_book(self):
+        resp_delete_book = self.app.test_client().delete('/books/2')
+        resp_get_book = self.app.test_client().get('/books')
+        self.assertEqual(resp_get_book.json, [self.book1])
+        self.assertEqual(resp_delete_book.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
